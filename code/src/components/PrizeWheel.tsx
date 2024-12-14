5f6b09e7279a516366d1f94424e7b29c629cafbf
benjanmin huang <benjaminhuanghuang@gmail.com>
2024-12-14 00:07:22 -0800

    update

--- code/src/components/PrizeWheel.tsx
+++ code/src/components/PrizeWheel.tsx
@@ -1,45 +1,167 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
-import { emails } from '../emails';
 import { Color } from '../types';
-import { randomColor } from '../utils';
+import { emails } from '../emails';
+import {
+  randomColors,
+  toRadius,
+  easeOutSine,
+  getPercent,
+  randomRotation,
+} from '../utils';
 import Popup from './Popup';
+// Assets
+import pointer from '../assets/pointer.svg';
 
-// const radius = 150; // Wheel radius
+const RADIUS = 400; // Wheel radius
 
 const PrizeWheel = () => {
-  const canvasRef = useRef(null);
-  const colors = useRef<Color[]>([]); // Colors for segments
+  console.log('PrizeWheel is re-render');
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const colorsRef = useRef<Color[]>([]);
+  const maxRotationRef = useRef(randomRotation());
+  const currentDegreeRef = useRef(0);
+
   const [winner, setWinner] = useState('');
+  const [speed, setSpeed] = useState<number>(0);
+  const itemDegs = useRef<{
+    [key: string]: { startDeg: number; endDeg: number };
+  }>({});
+
   const [isSpinning, setIsSpinning] = useState(false);
+
+  // Popup
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const openPopup = () => setIsPopupOpen(true);
   const closePopup = () => setIsPopupOpen(false);
 
-  //   const [currentDeg, setCurrentDeg] = useState(0);
-  //   const [speed, setSpeed] = useState(0);
-  //   const [pause, setPause] = useState(true);
+  const draw = useCallback(() => {
+    console.log('draw');
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-  //   const maxRotation = useRef(0);
-  //   const itemDegs = useRef({}); // Store segment start/end degrees
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
 
-  // Initialize colors for segments
-  useEffect(() => {
-    colors.current = Array(emails.length)
-      .fill(null)
-      .map(() => randomColor());
+    const width = canvas.width;
+    const height = canvas.height;
+    const centerX = width / 2;
+    const centerY = height / 2;
 
-    // Test
-    setWinner(emails[0].fullName);
+    const step = 360 / emails.length;
+    let startDeg = currentDegreeRef.current;
+
+    // Clear canvas
+    ctx.clearRect(0, 0, width, height);
+
+    // Draw background circle
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, RADIUS, toRadius(0), toRadius(360));
+    ctx.fillStyle = `rgb(33, 33, 33)`;
+    ctx.lineTo(centerX, centerY);
+    ctx.fill();
+
+    // Draw the wheel segments
+    for (let i = 0; i < emails.length; i++, startDeg += step) {
+      const endDeg = startDeg + step;
+      const color = colorsRef.current[i];
+      const colorStyle = `rgb(${color.r},${color.g},${color.b})`;
+      // slightly darker
+      const colorStyle2 = `rgb(${color.r - 30},${color.g - 30},${color.b - 30})`;
+
+      ctx.beginPath();
+      ctx.arc(
+        centerX,
+        centerY,
+        RADIUS - 2,
+        toRadius(startDeg),
+        toRadius(endDeg),
+      );
+      ctx.fillStyle = colorStyle2;
+      ctx.lineTo(centerX, centerY);
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.arc(
+        centerX,
+        centerY,
+        RADIUS - 30,
+        toRadius(startDeg),
+        toRadius(endDeg),
+      );
+      ctx.fillStyle = colorStyle;
+      ctx.lineTo(centerX, centerY);
+      ctx.fill();
+
+      // Draw text
+      ctx.save();
+      ctx.translate(centerX, centerY);
+      ctx.rotate(toRadius((startDeg + endDeg) / 2));
+      ctx.textAlign = 'center';
+      ctx.fillStyle =
+        color.r > 150 || color.g > 150 || color.b > 150 ? '#000' : '#fff';
+      ctx.font = 'bold 20px serif';
+      ctx.fillText(emails[i].fullName, 240, 10, 200);
+      ctx.restore();
+
+      // Record the start degree and end degree of each item
+      itemDegs.current[emails[i].email] = {
+        startDeg,
+        endDeg,
+      };
+
+      /* 
+      Check winner
+        startDeg lies in the bottom-right quadrant (270°–360°).
+        endDeg lies in the top-right quadrant (0°–90°).
+      */
+      if (
+        startDeg % 360 < 360 &&
+        startDeg % 360 > 270 &&
+        endDeg % 360 > 0 &&
+        endDeg % 360 < 90
+      ) {
+        setWinner(emails[i].fullName);
+        openPopup();
+        setIsSpinning(false);
+      }
+    }
   }, []);
 
-  const spin = () => {
-    console.log('Start Spin');
-    setIsSpinning(true);
-    openPopup();
+  const animate = () => {
+    // if (!isSpinning) return;
+    console.log("--------animate");
+    setSpeed(
+      easeOutSine(
+        getPercent(currentDegreeRef.current, maxRotationRef.current, 0),
+      ) * 20,
+    );
+
+    if (speed < 0.01) {
+      setSpeed(0);
+      setIsSpinning(false);
+    }
+    currentDegreeRef.current = currentDegreeRef.current + speed;
+    draw();
+    window.requestAnimationFrame(animate);
   };
+
+  const spin = () => {
+    setIsSpinning(true);
+    currentDegreeRef.current = 0;
+
+    draw();
+    maxRotationRef.current = randomRotation();
+    window.requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    colorsRef.current = randomColors(emails.length);
+    draw();
+  }, [draw]); // runs once after the component mounts
+
   return (
-    <div className=' bg-slate-950 flex-1 grid place-items-center relative'>
+    <div className='bg-slate-950 flex-1 grid place-items-center relative'>
       <Popup
         title='The Winner is:'
         message={winner}
@@ -48,18 +170,19 @@ const PrizeWheel = () => {
       />
       <canvas
         ref={canvasRef}
-        className='w-[800px] h-[800px] bg-red-300'
+        width='800'
+        height='800'
       />
       <div
         className={`w-[100px] h-[100px] rounded-full bg-white absolute m-auto
           ${isSpinning ? 'bg-slate-500 cursor-not-allowed pointer-events-none' : 'hover:bg-red-500 cursor-pointer'}`}
         onClick={() => spin()}
       ></div>
-      {/* The pointer */}
-      <div
-        className='w-0 h-0 border-t-[10px] border-b-[10px] border-r-[30px] border-t-transparent border-b-transparent 
-        border-r-white absolute mt-auto right-[400px]'
-      ></div>
+      <img
+        src={pointer}
+        alt='pointer'
+        className='absolute mt-auto right-[410px]'
+      />
     </div>
   );
 };
--- code/src/utils.ts
+++ code/src/utils.ts
@@ -1,15 +1,36 @@
-import { Color } from "./types";
+import { Color } from './types';
 
-export const toRad = (deg:number) => (deg * Math.PI) / 180;
+export const toRadius = (degree: number) => (degree * Math.PI) / 180;
 
-export const randomColor = () : Color => ({
-    r: Math.floor(Math.random() * 255),
-    g: Math.floor(Math.random() * 255),
-    b: Math.floor(Math.random() * 255)
-})
+const randomColor = (): Color => ({
+  r: Math.floor(Math.random() * 255),
+  g: Math.floor(Math.random() * 255),
+  b: Math.floor(Math.random() * 255),
+});
 
-export const randomRange = (min:number, max:number) => Math.floor(Math.random() * (max - min + 1)) + min;
+export const randomColors = (count: number) : Color[] => {
+  const colors: Color[] = [];
+  for (let i = 0; i < count; i++) {
+    colors.push(randomColor());
+  }
+  return colors;
+};
 
-export const easeOutSine = (x:number) => Math.sin((x * Math.PI) / 2);
+export const randomRange = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getPercent = (value:number, max:number, min:number) => (value - min) / (max - min);
+/*
+    An easing function that makes the motion start fast and then slow down gradually as it approaches the end.
+    x is typically a value between 0 and 1
+    0 (no movement initially)
+    1 (full movement at the end).
+*/
+export const easeOutSine = (x: number) => Math.sin((x * Math.PI) / 2);
+
+export const getPercent = (value: number, max: number, min: number) =>
+  (value - min) / (max - min);
+
+/*
+  result is within the desired range (1080 to 2160).
+*/
+export const randomRotation = () =>  Math.random() * (360 * 6 - 360 * 3) + 360 * 3;
